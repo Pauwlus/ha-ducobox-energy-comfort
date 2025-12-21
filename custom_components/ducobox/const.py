71b287f35e1cb66db3dc7de64f5d4c8f8d96d809
Pauwlus <42134561+Pauwlus@users.noreply.github.com>
2025-12-21 16:46:20 +0100

    Update const.py

--- custom_components/ducobox/const.py
+++ custom_components/ducobox/const.py
@@ -1,13 +1,14 @@
+# const.py
 DOMAIN = "ducobox"
+
+# standaard scan-interval (seconden)
 SCAN_INTERVAL = 45
 
-# REST endpoints
-DUCOBOX_NODE1 = "http://ducobox.localdomain/nodeinfoget?node=1"
-DUCOBOX_BOXINFO = "http://ducobox.localdomain/boxinfoget"
-
-DUCO_ZONE1 = "http://ducobox.localdomain/nodeinfoget?node=67"
-DUCO_ZONE2 = "http://ducobox.localdomain/nodeinfoget?node=68"
-
-DUCO_NODE2 = "http://ducobox.localdomain/nodeinfoget?node=2"
-DUCO_NODE3 = "http://ducobox.localdomain/nodeinfoget?node=3"
-DUCO_NODE4 = "http://ducobox.localdomain/nodeinfoget?node=4"
+# REST endpoints (host via config entry)
+DUCOBOX_NODE1   = "http://{host}/nodeinfoget?node=1"
+DUCOBOX_BOXINFO = "http://{host}/boxinfoget"
+DUCO_ZONE1      = "http://{host}/nodeinfoget?node=67"
+DUCO_ZONE2      = "http://{host}/nodeinfoget?node=68"
+DUCO_NODE2      = "http://{host}/nodeinfoget?node=2"
+DUCO_NODE3      = "http://{host}/nodeinfoget?node=3"
+DUCO_NODE4      = "http://{host}/nodeinfoget?node=4"
